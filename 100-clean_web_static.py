13190f23bbec6614f3a139774bc7c2fec2d589d7
Foster Setor <walkerdziwornu@gmail.com>
2024-05-04 03:05:40 -0700

    deployment resolved-fab-py_ws

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""
 # fabfile deleting out-of-date archives.*
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ['100.25.153.16', '100.25.156.205']
 
 
 def do_clean(number=0):
-    """Deleting out-of-date archives.
-
-    Args>
-        number (int)> The number of archives to keep.
-
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,54 +1,62 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+Fabric script for deploying web_static content to web servers.
 
-execute: fab -f 3-deploy_web_static.py deploy -i ~/root/.ssh/school -u ubuntu
+Usage: fab -f 3-deploy_web_static.py deploy -i /root/.ssh/school -u ubuntu
 """
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
+
+# Configure Fabric environment
+env.user = 'ubuntu'
+env.key_filename = '/root/.ssh/school'
 env.hosts = ['100.25.153.16', '100.25.156.205']
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """Generates a compressed archive of web_static directory."""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
+        if not isdir("versions"):
+            local("mkdir -p versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception as e:
+        print("Error in packaging: {}".format(e))
         return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to the web servers and sets up symbolic link."""
+    if not exists(archive_path):
+        print("Archive not found: {}".format(archive_path))
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception as e:
+        print("Error in deployment: {}".format(e))
         return False
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """Main deployment function that creates and distributes the archive."""
     archive_path = do_pack()
-    if archive_path is None:
+    if archive_path:
+        return do_deploy(archive_path)
+    else:
         return False
-    return do_deploy(archive_path)
+
