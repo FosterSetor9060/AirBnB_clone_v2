0529a90a90fd4823c43a0d25e072073f2f608273
Foster Setor <walkerdziwornu@gmail.com>
2024-05-21 11:07:00 +0000

    Deployment && Web_app listening on Port 5000

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -15,21 +15,21 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displaying 'Hello HBNB!'."""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displaying 'HBNB'."""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displaying 'C' followed by the value of <text>.
+    """Displays 'C' followed by the value of <text>.
 
-    Replacing any underscores in <text> with slashes.
+    Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
     return "C {}".format(text)
@@ -38,13 +38,13 @@ def c(text):
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """Displaying 'Python' followed by the value of <text>.
+    """Displays 'Python' followed by the value of <text>.
 
-    Replaceing any underscores in <text> with slashes.
+    Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000)
