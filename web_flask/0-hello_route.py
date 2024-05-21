3b783bf2077d93b4b8ca81f1e83a9b33c8dc07ca
Foster Setor <walkerdziwornu@gmail.com>
2024-05-21 10:54:46 +0000

    deployment & web_app listening to P0rt_500

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,16 +5,21 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /=>  Displaying 'Hello HBNB!'
 """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displaying 'Hello HBNB!'"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,22 +6,21 @@ Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb=> Displays 'HBNB'.
 """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displaying 'Hello HBNB!'."""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displaying 'HBNB'."""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
